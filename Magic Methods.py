552b400e52de5f4a6abf59c162668c034ba743b0
simmer5 <strumke@yahoo.com>
2019-05-02 11:58:10 +0200

    Add files via upload

---
+++ Magic Methods.py
@@ -0,0 +1,95 @@
+"""
+__add__ for +
+__sub__ for -
+__mul__ for *
+__truediv__ for /
+__floordiv__ for //
+__mod__ for %
+__pow__ for **
+__and__ for &
+__xor__ for ^
+__or__ for |
+
+The expression x + y is translated into x.__add__(y).
+However, if x hasn't implemented __add__, and x and y are of different types, then y.__radd__(x) is called.
+There are equivalent r methods for all magic methods just mentioned.
+
+===============================
+
+Python also provides magic methods for comparisons.
+__lt__ for <
+__le__ for <=
+__eq__ for ==
+__ne__ for !=
+__gt__ for >
+__ge__ for >=
+
+If __ne__ is not implemented, it returns the opposite of __eq__.
+There are no other relationships between the other operators.
+
+===========================================
+
+There are several magic methods for making classes act like containers.
+__len__ for len()
+__getitem__ for indexing
+__setitem__ for assigning to indexed values
+__delitem__ for deleting indexed values
+__iter__ for iteration over objects (e.g., in for loops)
+__contains__ for in
+
+There are many other magic methods that we won't cover here, 
+such as __call__ for calling objects as functions, 
+and __int__, __str__, and the like, for converting objects to built-in types.
+
+__del__ for del
+__repr__ used for string representation of the instance
+
+"""
+class SpecialString:
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __truediv__(self, other):
+        line = "=" * len(other.cont)
+        return "\n".join([self.cont, line, other.cont])
+
+spam = SpecialString("spam")
+hello = SpecialString("Hello world!")
+by = SpecialString("Ate")
+print(hello / spam)
+
+#=================================================
+
+class SpecialString:
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __gt__(self, other):
+        for index in range(len(other.cont)+1):
+            result = other.cont[:index] + ">" + self.cont
+            result += ">" + other.cont[index:]
+            print(result)
+
+spam = SpecialString("spam")
+eggs = SpecialString("eggs")
+spam > eggs
+
+#===========================================
+
+import random
+
+class VagueList:
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __getitem__(self, index):
+        return self.cont[index + random.randint(-1, 1)]
+
+    def __len__(self):
+        return random.randint(0, len(self.cont)*2)
+
+vague_list = VagueList(["A", "B", "C", "D", "E"])
+print(len(vague_list))
+print(len(vague_list))
+print(vague_list[2])
+print(vague_list[2])
